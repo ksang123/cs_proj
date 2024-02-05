c93c3230a0adafce0ef159366206c84eb7803c35
ksang123 <etaizilberman@gmail.com>
2024-02-05 10:27:35 +0200

    auto commit 05-02 10:27

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, world!");
+    printf("Hello, world!\n");
+    for (int i = 0; i < 3; i++) {
+        printf("hello\n");
+    }
     return 0;
 }
