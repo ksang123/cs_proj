8b1e70984b6625b45062b47218e81a77b0bcffaa
ksang123 <etaizilberman@gmail.com>
2024-03-20 16:51:11 +0200

    auto commit 20-03 16:51

--- main.c
+++ main.c
@@ -16,12 +16,12 @@ bool is_perm(char str1[], char str2[], int n, char or[]) {
     if (n == 0) {
         return false;
     }
-    snprintf(or, strlen(str1), "%s", str1);
     for (int i = 0; i <= n; i++) {
         swap(str1, i, n);
         if (is_perm(str1, str2, n-1, or)) {
             return true;
         }
+
     }
     return false;
 }
