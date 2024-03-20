eb9d3167066c4a0ef58f32eb8c1c14b808e074d6
ksang123 <etaizilberman@gmail.com>
2024-03-20 16:59:50 +0200

    auto commit 20-03 16:59

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <stdlib.h>
 
 void swap(char str[], int idx1, int idx2) {
     char temp = str[idx1];
@@ -9,21 +8,22 @@ void swap(char str[], int idx1, int idx2) {
     str[idx2] = temp;
 }
 
-bool is_perm(char str1[], char str2[], int n, char or[]) {
+bool is_perm(char str1[], char str2[], int n) {
     if (strcmp(str1, str2) == 0) {
         return true;
     }
     if (n == 0) {
         return false;
     }
-//    snprintf(or, strlen(str1)+1, "%s", str1);
     for (int i = 0; i <= n; i++) {
-        swap(str1, i, n);
-        if (is_perm(str1, str2, n-1, or)) {
-//            snprintf(str1, strlen(str1)+1, "%s", or);
+        if (n % 2 == 0) {
+            swap(str1, 0, n);
+        } else {
+            swap(str1, i, n);
+        }
+        if (is_perm(str1, str2, n-1)) {
             return true;
         }
-//        snprintf(str1, strlen(str1)+1, "%s", or);
     }
     return false;
 }
@@ -33,9 +33,7 @@ bool isPermutation(char str1[], char str2[]) {
     if (len1 != len2) {
         return false;
     }
-    char* str = (char*) malloc(sizeof(char) * strlen(str1));
-    bool is_permutation = is_perm(str1, str2, len1-1, str);
-    free(str);
+    bool is_permutation = is_perm(str1, str2, len1-1);
     return is_permutation;
 }
 
