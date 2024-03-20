ced6634b926788b4c455c10f0eec1171034c9c00
ksang123 <etaizilberman@gmail.com>
2024-03-20 16:40:30 +0200

    auto commit 20-03 16:40

--- main.c
+++ main.c
@@ -13,7 +13,6 @@ char* swap(char str[], int idx1, int idx2) {
 }
 
 bool is_perm(char str1[], char str2[], int n) {
-    printf("%s\n", str1);
     if (strcmp(str1, str2) == 0) {
         return true;
     }
