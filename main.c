aea268cad63e9d1da81e77278d1b47630bc5ddb3
ksang123 <etaizilberman@gmail.com>
2024-03-20 15:31:45 +0200

    auto commit 20-03 15:31

--- main.c
+++ main.c
@@ -11,9 +11,10 @@
 void printSong(char *songLyrics[], int songSize);
 int readSongLyrics(char *songLyrics[], int songMaxSize, int wordMaxLen);
 void releaseMemory(char *songLyrics[], int songSize);
-//int* findSwiftyWordIndexes(char *songLyrics[], int songSize, int k);
-//void encryptSwiftyWord(char *songLyrics[], int wordIdxs[], int k,
-//unsigned char taylorCipherKey[ABC_SIZE]);
+int* findSwiftyWordIndexes(char *songLyrics[], int songSize, int k);
+void encryptSwiftyWord(char *songLyrics[], int wordIdxs[], int k,
+unsigned char taylorCipherKey[ABC_SIZE]);
+int find_word_k(char *songLyrics[], int songSize, int k);
 
 
 // DO NOT TOUCH
@@ -29,10 +30,10 @@ void printSong(char *songLyrics[], int songSize) {
 // DO NOT TOUCH
 int main(void)
 {
-//    unsigned char taylorCipherKey[ABC_SIZE] = {
-//            '@', '4', '(', '8', '3', '/', '6', 'H', '!', '}', '&', '1', '+',
-//            '^', '0', '5', '9', 'R', '$', '#', '<', '>', 'w', '*', '~', 'z'
-//    };
+    unsigned char taylorCipherKey[ABC_SIZE] = {
+            '@', '4', '(', '8', '3', '/', '6', 'H', '!', '}', '&', '1', '+',
+            '^', '0', '5', '9', 'R', '$', '#', '<', '>', 'w', '*', '~', 'z'
+    };
 
     char *songLyrics[SONG_MAX_SIZE] = { NULL };
 
@@ -47,14 +48,14 @@ int main(void)
 
     // Find the word that appears k times in the song,
     // and encrypt it using the key
-//    int *wordIdxs = findSwiftyWordIndexes(songLyrics, songSize, k);
-//    encryptSwiftyWord(songLyrics, wordIdxs, k, taylorCipherKey);
+    int *wordIdxs = findSwiftyWordIndexes(songLyrics, songSize, k);
+    encryptSwiftyWord(songLyrics, wordIdxs, k, taylorCipherKey);
 
     // Print the result
     printSong(songLyrics, songSize);
 
     // Release all dynamically allocated memory
-//    free(wordIdxs);
+    free(wordIdxs);
     releaseMemory(songLyrics, songSize);
 
     return 0;
@@ -62,24 +63,14 @@ int main(void)
 
 /* Your Implementation: */
 int readSongLyrics(char* songLyrics[], int songMaxSize, int wordMaxLen) {
-    int word_counter = 0;
-    int len;
+    int word_counter = 0, len;
     char* word = (char *)malloc(wordMaxLen * sizeof(char));
-    if (word == NULL) {
-        printf("memory allocation failed\n");
-        return 0;
-    }
     while (word_counter < songMaxSize && scanf("%s", word) != -1) // || add empty input)
     {
         len = (int)strlen(word);
         word[len] = ' ';
         word[len+1] = '\0';
-        printf("%d", len);
         songLyrics[word_counter] = (char *)malloc((len+2) * sizeof(char));
-        if (songLyrics[word_counter] == NULL) {
-            printf("memory allocation failed\n");
-            return 0;
-        }
         strcpy(songLyrics[word_counter], word);
         word_counter++;
     }
@@ -90,5 +81,45 @@ int readSongLyrics(char* songLyrics[], int songMaxSize, int wordMaxLen) {
 void releaseMemory(char *songLyrics[], int songSize) {
     for (int i = 0; i < songSize; i++) {
         free(songLyrics[i]);
+    }
+}
+
+int* findSwiftyWordIndexes(char *songLyrics[], int songSize, int k) {
+    int *wordIdxs = (int *) malloc(sizeof(int) * k),
+    idx = find_word_k(songLyrics, songSize, k), counter = 0;
+    for (int i = 0; i< songSize; i++) {
+        if (strcmp(songLyrics[i], songLyrics[idx]) == 0) {
+            wordIdxs[counter] = i;
+            counter++;
+        }
+    }
+    return wordIdxs;
+}
+
+int find_word_k(char *songLyrics[], int songSize, int k) {
+    int counter;
+    for (int i = 0; i < songSize; i++) {
+        counter = 0;
+        for (int j = 0; j < songSize; j++) {
+            if (strcmp(songLyrics[i], songLyrics[j]) == 0) {
+                counter++;
+            }
+        }
+        if (counter == k) {
+            return i;
+        }
+    }
+    printf("whoopsy poopsy word not found\n");
+    return -1;
+}
+
+void encryptSwiftyWord(char *songLyrics[], int wordIdxs[], int k,
+                       unsigned char taylorCipherKey[ABC_SIZE]) {
+    for (int i = 0; i < k; i++) {
+        int len = (int)strlen(songLyrics[wordIdxs[i]]);
+        for (int j = 0; j < len; j++) {
+            songLyrics[wordIdxs[i]][j] = (char)taylorCipherKey[
+                    songLyrics[wordIdxs[i]][j] - 'a' + 1];
+        }
     }
 }
