7f31ac0bf4d4b942fd543a9ea0ee54d62d8ebcda
ksang123 <etaizilberman@gmail.com>
2024-03-20 14:51:03 +0200

    auto commit 20-03 14:51

---
+++ hw4/q2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int question;
+    char complexity;
+    scanf("%d", &question);
+    scanf("%c", &complexity);
+    if (complexity == 't'){
+        switch (question) {
+            case 1:
+                printf("The time complexity of f1 is: %s\n" , "a");
+                break;
+            case 2:
+                printf("The time complexity of f2 is: %s\n" , "a");
+                break;
+            case 3:
+                printf("The time complexity of f3 is: %s\n" , "a");
+                break;
+            case 4:
+                printf("The time complexity of f4 is: %s\n" , "a");
+                break;
+            default:
+                printf("Error!\n");
+                break;
+        }
+    }
+    if (complexity == 's'){
+        switch (question) {
+            case 1:
+                printf("The space complexity of f1 is: %s\n" , "a");
+                break;
+            case 2:
+                printf("The space complexity of f2 is: %s\n" , "a");
+                break;
+            case 3:
+                printf("The space complexity of f3 is: %s\n" , "a");
+                break;
+            case 4:
+                printf("The space complexity of f4 is: %s\n" , "a");
+                break;
+            default:
+                printf("Error!\n");
+                break;
+        }
+    }
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,95 +1,77 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-//the function gets a non-negative number
-//the function returns its finite digit sum
-int midterm_q1(int num);
+#define ABC_SIZE 'z' - 'a' + 1
+#define WORD_MAX_LEN 12
+#define SONG_MAX_SIZE 50
 
-//the function gets a non-negative number and returns the sum of its digits
-int sum_digits(int num);
+/* Function Declarations */
+void printSong(char *songLyrics[], int songSize);
+int readSongLyrics(char *songLyrics[], int songMaxSize, int wordMaxLen);
+//void releaseMemory(char *songLyrics[], int songSize);
+//int* findSwiftyWordIndexes(char *songLyrics[], int songSize, int k);
+//void encryptSwiftyWord(char *songLyrics[], int wordIdxs[], int k,
+//unsigned char taylorCipherKey[ABC_SIZE]);
 
-//the function gets an array and the length of the array
-//the function returns the 2nd smallest number in the array
-double midterm_q2(double arr[], int n);
 
-//the function gets an array, the length of the array and an index
-//the function calculates the sliding sum of the array at the index
-double midterm_q3(double arr[], int n, int idx);
+// DO NOT TOUCH
+void printSong(char *songLyrics[], int songSize) {
+    for (int i = 0; i < songSize; i++) {
+        if (songLyrics[i] != NULL) {
+            printf("%s", songLyrics[i]);
+        }
+    }
+    printf("\n");
+}
 
-/* the function gets an array, the length of the array,
- * the index of the sliding mean, the current index,
- * and the points that this index is worth.
- * the function returns how much should the sliding sum increase from
- * the specific index
- * */
-double add_to_sum(double arr[], int n, int idx, int index, double points);
+// DO NOT TOUCH
+int main(void)
+{
+//    unsigned char taylorCipherKey[ABC_SIZE] = {
+//            '@', '4', '(', '8', '3', '/', '6', 'H', '!', '}', '&', '1', '+',
+//            '^', '0', '5', '9', 'R', '$', '#', '<', '>', 'w', '*', '~', 'z'
+//    };
 
-int main(void) {
-    int num = 0;
-    printf("%d\n", midterm_q1(num));
-    double arr[] = {-5.5, 0.0, 6.0};
-    printf("%f\n", midterm_q2(arr, 3));
+    char *songLyrics[SONG_MAX_SIZE] = { NULL };
+
+    // Get k input
+    int k;
+    printf("Enter k:\n");
+    scanf("%d", &k);
+
+    // Get song lyrics input
+    printf("Enter your song lyrics:\n");
+    int songSize = readSongLyrics(songLyrics, SONG_MAX_SIZE, WORD_MAX_LEN);
+
+    // Find the word that appears k times in the song,
+    // and encrypt it using the key
+//    int *wordIdxs = findSwiftyWordIndexes(songLyrics, songSize, k);
+//    encryptSwiftyWord(songLyrics, wordIdxs, k, taylorCipherKey);
+
+    // Print the result
+    printSong(songLyrics, songSize);
+
+    // Release all dynamically allocated memory
+//    free(wordIdxs);
+//    releaseMemory(songLyrics, songSize);
+
     return 0;
 }
 
-int midterm_q1(int num) {
-    if (num < 0) { //make sure that the number is non-negative
-        printf("ERROR: negative number inserted");
-        return -1;
+/* Your Implementation: */
+int readSongLyrics(char* songLyrics[], int songMaxSize, int wordMaxLen) {
+    int word_counter = 0;
+    char* word = (char *)malloc(wordMaxLen * sizeof(char));
+    if (word == NULL) {
+        printf("memory allocation failed");
     }
-    int sum = sum_digits(num); //the sum of the digits
-    while (sum >= 10) { //while the sum has 2 or more digits
-        //calculate the sum of the new number
-        sum = sum_digits(sum);
+    while (word_counter < songMaxSize && scanf("%s", word) != -1) // || add empty input)
+    {
+        strcpy(songLyrics[word_counter], word);
+        word_counter++;
     }
-    return sum;
-}
-
-int sum_digits(int num) {
-    int sum = 0; //the sum of the digits
-    while (num > 0) { //loop while the num still has digits left
-        sum += num % 10; //add the last digit to the sum
-        num /= 10; //remove the last digit
-    }
-    return sum;
-}
-
-double midterm_q2(double arr[], int n) {
-    //set the smallest and 2nd smallest numbers in the array
-    double smallest = arr[0], second_smallest = arr[0];
-    //loop through the array and find the smallest number
-    for (int i =1; i < n; i++) {
-        if (smallest > arr[i]) {
-            smallest = arr[i];
-        }
-    }
-    //loop through the array and find the smallest number that is
-    //different from the true smallest number
-    //that would be the 2nd smallest number
-    for (int i = 0; i < n; i++) {
-        //if smallest equals to the 2nd smallest, switch the 2nd smallest
-        if ((arr[i] != smallest && second_smallest > arr[i]) ||
-            smallest == second_smallest) {
-            second_smallest = arr[i];
-        }
-    }
-    return second_smallest;
-}
-
-double midterm_q3(double arr[], int n, int idx) {
-    double sum = 0; //the sliding sum
-    //add the items in the sliding sum, with their specific worth
-    sum += add_to_sum(arr, n, idx, -2, 0.1);
-    sum += add_to_sum(arr, n, idx, -1, 0.2);
-    sum += add_to_sum(arr, n, idx, 0, 0.4);
-    sum += add_to_sum(arr, n, idx, 1, 0.2);
-    sum += add_to_sum(arr, n, idx, 2, 0.1);
-    return sum;
-}
-
-double add_to_sum(double arr[], int n, int idx, int index, double points) {
-    //check that the index is in the array
-    if (idx + index >= 0 && idx + index < n) {
-        return arr[idx + index] * points; //return the value of the index
-    }
-    return 0.0; //the index was not in the array
+    free(word);
+    return word_counter;
 }
