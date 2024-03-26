7f77c9094bd8559f3303ab65262b0fd4b196a66f
ksang123 <etaizilberman@gmail.com>
2024-03-26 22:21:22 +0200

    auto commit 26-03 22:21

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 const char* one2twenty[] = {
@@ -10,34 +11,28 @@ const char* tens[] = {
         "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty",
         "Ninety"
 };
+
 char* int2string(int num) {
-    printf("%d\n", num);
-    // Assuming the result will fit within 100 characters
-    static char result[100];
-    memset(result, 0, sizeof(result)); // Initialize the result string
+    // Allocate memory for the result string
+    char* result = (char*)malloc(100);
+    if (result == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+
     if (num == 0) {
-        return "Zero";
-    }
-    if (num < 20) {
-        sprintf(result, "%s", one2twenty[num - 1]);
-        return result;
-    }
-    if (num < 100) {
+        strcpy(result, "");
+    } else if (num < 20) {
+        sprintf(result, "%s ", one2twenty[num - 1]);
+    } else if (num < 100) {
         sprintf(result, "%s %s", tens[num / 10 - 2], int2string(num % 10));
-        return result;
+    } else if (num < 1000) {
+        sprintf(result, "%s Hundred %s", one2twenty[num / 100 - 1], int2string(num % 100));
+    } else {
+        sprintf(result, "%s Thousand %s", one2twenty[num / 1000 - 1], int2string(num % 1000));
     }
-    if (num < 1000) {
-        sprintf(result, "%s Hundred %s", one2twenty[num / 100 - 1],
-                int2string(num % 100));
-        return result;
-    }
-    if (num < 10000) {
-        sprintf(result, "%s Thousand %s", int2string(num / 1000),
-                int2string(num % 1000));
-        return result;
-    }
-    // Add support for larger numbers here if needed
-    return "Number too large";
+
+    return result;
 }
 
 int main(void) {
@@ -50,5 +45,6 @@ int main(void) {
     }
     char* result = int2string(num);
     printf("%s\n", result);
+    free(result);  // Free the dynamically allocated memory
     return 0;
 }
