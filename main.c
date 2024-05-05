26b4fb0c93679956e80fab24f677b9387c25d1a8
ksang123 <etaizilberman@gmail.com>
2024-05-05 20:49:20 +0300

    auto commit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int search(int arr[], int n, int target);
 
 int main(void) {
     int arr[] = {1,3,4,5,7,9,10};
-    printf("%d",q2(arr, 7, 3, 5));
+    printf("%d\n",q2(arr, 7, 3, 5));
     return 0;
 }
 
@@ -15,8 +15,8 @@ bool q2(int arr[], int n, int min, int max) {
     if (start == -1) {
         return false;
     }
-    printf("%d", arr[start + min - max]);
-    printf("%d", max - min);
+    printf("%d\n", arr[start + min - max]);
+    printf("%d\n", max - min);
     return arr[start + min - max] == max - min;
 }
 
