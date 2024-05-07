0b0afb1dbdd8749e1987c58024a79a3a0a8c9e80
ksang123 <etaizilberman@gmail.com>
2024-05-07 11:47:14 +0300

    auto commit 07-05 11:47

--- exams/recursion_and_shit.c
+++ exams/recursion_and_shit.c
@@ -16,7 +16,48 @@ void hawaii1(int m, int from, int to) {
     hawaii1(m-1, from, to);
     movered(from, via);
     hawaii1(m-1, to, via);
-    movewhite(to, from);
+    movewhite(from, to);
     hawaii1(m-1, via, from);
     movered(via, to);
+    hawaii1(m-1, from, to);
+}
+
+void hawaii2(int m, int from, int to) {
+    if (m == 0) {
+        return;
+    }
+    int via = 6 - from - to;
+    hawaii1(m - 1, from, to);
+    movered(from, via);
+    hawaii1(m - 1, to, via);
+    movewhite(from, to);
+    hawaii1(m - 1, via, to);
+    movered(via, from);
+    hawaii1(m - 1, to, from);
+    hawaii2(m - 1, from, to);
+}
+
+void print_arr(int x[], int idx) {
+    for (int i = 0; i < idx; i++) {
+        printf("%d, ", x[i]);
+    }
+    printf("\n");
+}
+
+int printSolution(int a[], int n, int b, int idx, int x[]) {
+    if (idx == n) {
+        if (b == 0) {
+            print_arr(x, idx);
+            return 0;
+        }
+        return -1;
+    }
+    int sol = -1;
+    for(int i = 0; i <= b / a[idx]; i++) {
+        x[idx] = i;
+        if (!printSolution(a, n, b - x[idx] * a[idx], idx + 1, x)) {
+            sol = 0;
+        }
+    }
+    return sol;
 }
--- main.c
+++ main.c
@@ -1,44 +1,34 @@
 #include <stdio.h>
 
-void movered(int from, int to) {
-    printf("move red ring from %d to %d\n", from, to);
-}
-
-void movewhite(int from, int to) {
-    printf("move white ring from %d to %d\n", from, to);
-}
-
-void hawaii1(int m, int from, int to) {
-    if (m == 0) {
-        return;
+void print_arr(int x[], int idx) {
+    for (int i = 0; i < idx; i++) {
+        printf("%d, ", x[i]);
     }
-    int via = 6 - from - to;
-    hawaii1(m-1, from, to);
-    movered(from, via);
-    hawaii1(m-1, to, via);
-    movewhite(from, to);
-    hawaii1(m-1, via, from);
-    movered(via, to);
-    hawaii1(m-1, from, to);
+    printf("\n");
 }
 
-void hawaii2(int m, int from, int to) {
-    if (m == 0) {
-        return;
+int printSolution(int a[], int n, int b, int idx, int x[]) {
+    if (idx == n) {
+        if (b == 0) {
+            print_arr(x, idx);
+            return 0;
+        }
+        return -1;
     }
-    int via = 6 - from - to;
-    hawaii1(m - 1, from, to);
-    movered(from, via);
-    hawaii1(m - 1, to, via);
-    movewhite(from, to);
-    hawaii1(m - 1, via, to);
-    movered(via, from);
-    hawaii1(m - 1, to, from);
-    hawaii2(m - 1, from, to);
+    int sol = -1;
+    for(int i = 0; i <= b / a[idx]; i++) {
+        x[idx] = i;
+        if (!printSolution(a, n, b - x[idx] * a[idx], idx + 1, x)) {
+            sol = 0;
+        }
+    }
+    return sol;
 }
 
 
 int main(void) {
-    hawaii2(2, 1, 3);
+    int a[] = {3, 4, 6, 2};
+    int x[4] = {0};
+    printf("%d\n", printSolution(a, 4, 26, 0, x));
     return 0;
 }
