203b7ef02935280aa484db22b6d9271ccf6d64f6
ksang123 <etaizilberman@gmail.com>
2024-05-08 00:09:57 +0300

    auto commit

--- algorithms/merge_sort.c
+++ algorithms/merge_sort.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 void merge(int a[], int na, int b[], int nb, int c[]) {
     int ia, ib, ic;
@@ -35,4 +36,27 @@ void merge_sort(int a[], int n) {
     int *tmp_array = (int*) malloc(sizeof(int) * n);
     internal_msort(a, n, tmp_array);
     free(tmp_array);
+}
+
+
+void print_array(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d, ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main(void) {
+    int arr[] = {5, 2, 9, 1, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    print_array(arr, n);
+
+    merge_sort(arr, n);
+
+    printf("Sorted array: ");
+    print_array(arr, n);
+
+    return 0;
 }
